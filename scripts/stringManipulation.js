956608048548e4a4abdd90de78d58cba9d25ec53
defycab <love.bellinder@gmail.com>
2024-09-26 00:40:37 +0200

    longestword attempt

--- scripts/stringManipulation.js
+++ scripts/stringManipulation.js
@@ -12,14 +12,34 @@ console.log(joinedReversedString)
 
 // palindrome challenge
 
-// function is case sensitive
+// Problems: function is case sensitive
 function isPalindrome(str) {
-    const reversedString = str.split("").reverse().join("")
-    if(str === reversedString) {
-        return `${str} is a palindrome` 
-    } else {
-        return `${str} is NOT a palindrome`
-    }
+  const reversedString = str.split("").reverse().join("")
+  if (str === reversedString) {
+    return `${str} is a palindrome`
+  } else {
+    return `${str} is NOT a palindrome`
+  }
 }
 
 console.log(isPalindrome("abba"))
+
+// Problems: function inlcludes dot in word-length
+function findLongestWord(sentence) {
+  const words = sentence.split(" ")
+  let longestWord = ""
+  let counter = 0
+
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word
+    }
+    counter++
+  }
+  return longestWord
+}
+
+const sentenceToBeTested =
+  "Love är ett vackert namn som finns i sagorna. Jag vet inte varför det skulle vara så egendomligt"
+
+console.log(findLongestWord(sentenceToBeTested))
