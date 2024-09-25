0b5c8f6b2f3fa9a231733b3491310e2984d5c3a7
defycab <love.bellinder@gmail.com>
2024-09-26 00:30:59 +0200

    adding stringManipulation resource

---
+++ scripts/stringManipulation.js
@@ -0,0 +1,11 @@
+const string = "Jag heter Love Bellinder"
+
+// reverse string
+
+let splitString = string.split("")
+
+let splitReversedString = splitString.reverse()
+
+let joinedReversedString = splitReversedString.join("")
+
+console.log(joinedReversedString)
